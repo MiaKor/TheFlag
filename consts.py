23e4d66de85fa8ce1ff2d7601a189b45ad7cd4aa
Mia koren <miakorenk2@gmail.com>
2025-08-24 11:54:29 +0300

    consts change2

--- consts.py
+++ consts.py
@@ -1,21 +1,22 @@
 import pygame
+import os
 
 pygame.init()
 
 SCREEN_GRID_START_ROWS = 25
 SCREEN_GRID_COLS = 50
 WINDOW_HEIGHT = 750
-WINDOW_WIDTH=1000
+WINDOW_WIDTH=1500
 
-SOLDIER = pygame.image.load('bin','soldier.png')
-SOLDIER_NIGHT = pygame.image.load('bin','soldier_night.png')
-FLAG = pygame.image.load('bin','flag.png')
-EXPLOSION = pygame.image.load('bin','explotion.png')
-MINE = pygame.image.load('bin','mine.png')
-INJURY = pygame.image.load('bin','injury.png')
-SNAKE = pygame.image.load('bin','snake.png')
-GRASS = pygame.image.load('bin','grass')
-SOLDIER_2 = pygame.image.load('bin','soldier (2).png')
+SOLDIER = pygame.image.load(os.path.join('bin','soldier.png'))
+SOLDIER_NIGHT = pygame.image.load(os.path.join('bin','soldier_night.png'))
+FLAG = pygame.image.load(os.path.join('bin','flag.png'))
+EXPLOSION = pygame.image.load(os.path.join('bin','explotion.png'))
+MINE = pygame.image.load(os.path.join('bin','mine.png'))
+INJURY = pygame.image.load(os.path.join('bin','injury.png'))
+SNAKE = pygame.image.load(os.path.join('bin','snake.png'))
+GRASS = pygame.image.load(os.path.join('bin','grass'))
+SOLDIER_2 = pygame.image.load(os.path.join('bin','soldier (2).png'))
 
 REGULAR_BACKGROUND = (138, 201, 38)
 BACKGROUND_WHEN_ENTER = (0, 0, 0)
