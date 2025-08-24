964551ddaa8d754546256ddb0138ae3d2047a300
miashats <shklovski2@gmail.com>
2025-08-24 12:09:46 +0300

    screen_green

--- screen.py
+++ screen.py
@@ -4,3 +4,6 @@ import pygame
 pygame.init()
 screen = pygame.display.set_mode((consts.WINDOW_WIDTH, consts.WINDOW_HEIGHT))
 
+def draw_window():
+    screen.fill(consts.REGULAR_BACKGROUND)
+    pygame.display.update()
