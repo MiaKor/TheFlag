e732b8f20f11c09822feeded4aa34e1830977c0c
miashats <shklovski2@gmail.com>
2025-08-24 11:59:04 +0300

    screen

--- screen.py
+++ screen.py
@@ -1,1 +1,11 @@
+import consts
 import pygame
+
+pygame.init()
+screen = pygame.display.set_mode((consts.WINDOW_WIDTH, consts.WINDOW_HEIGHT))
+run= True
+while run:
+    for event in pygame.event.get():
+        if event.type==pygame.QUIT:
+            run= False
+pygame.quit()
