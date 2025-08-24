eb40d85a98156a94885bee6ee68f844e5d8c4003
hello <ayab1142007@gamil.com>
2025-08-24 11:30:08 +0300

    CONSTANTS

---
+++ consts.py
@@ -0,0 +1,20 @@
+import pygame
+
+pygame.init()
+
+SCREEN_GRID_START_ROWS = 25
+SCREEN_GRID_COLS = 50
+
+SOLDIER = pygame.image.load('soldier.png')
+SOLDIER_NIGHT = pygame.image.load('soldier_night.png')
+FLAG = pygame.image.load('flag.png')
+EXPLOSION = pygame.image.load('explotion.png')
+MINE = pygame.image.load('mine.png')
+INJURY = pygame.image.load('injury.png')
+SNAKE = pygame.image.load('snake.png')
+GRASS = pygame.image.load('grass')
+SOLDIER_2 = pygame.image.load('soldier (2).png')
+
+REGULAR_BACKGROUND = (138, 201, 38)
+BACKGROUND_WHEN_ENTER = (0, 0, 0)
+LIGHT_GREEN = (144, 238, 144)
