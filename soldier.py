55cbbb06f5dff11be4219f7a5e08857aa8d48a55
Mia koren <miakorenk2@gmail.com>
2025-08-24 16:53:04 +0300

    start soldier

--- soldier.py
+++ soldier.py
@@ -1,13 +1,12 @@
 import pygame
-def soldier():
-
-
-
-        # Create a box to put the arrow in, so that the rotation will be around
-        # it's bottom (the box's center)
-        soldier_box = pygame.Surface(
-                (consts.ARROW_WIDTH, consts.ARROW_HEIGHT * 2), )
-        arrow_box.fill(consts.BACKGROUND_COLOR)
-        arrow_box.blit(sized_arrow, (0, 0))
-
-        return arrow_box
+# def soldier():
+#
+#
+#
+#         # Create a box to put the arrow in, so that the rotation will be around
+#         # it's bottom (the box's center)
+#         soldier_box = pygame.Surface(,(30, 60))
+#         arrow_box.fill(consts.BACKGROUND_COLOR)
+#         arrow_box.blit(sized_arrow, (0, 0))
+#
+#         return arrow_box
