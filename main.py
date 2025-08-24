7c1f03bb9577ef26d08f51cbece554b56690dde7
Mia koren <miakorenk2@gmail.com>
2025-08-24 16:52:47 +0300

    main change

--- main.py
+++ main.py
@@ -4,23 +4,26 @@ import screen
 import game_field
 import soldier
 
-state = {"state": consts.RUNNING_STATE, "is_window_open": True}
+state = {"state": consts.RUNNING_STATE, "is_window_open": True, 'night': False}
 
 
 def main():
     pygame.init()
-    screen.draw_night_window()
-    # screen.draw_window()
+
+    screen.draw_window()
     screen.draw_grass()
+    screen.draw_solider()
+    screen.draw_flag()
     while state["is_window_open"]:
         handle_user_events()
+        if state['night']:
+            screen.draw_night_window()
+            continue
 
         if is_lose():
             state["state"] = consts.LOSE_STATE
         elif is_win():
             state["state"] = consts.WIN_STATE
-
-
 
 
 def handle_user_events():
@@ -29,17 +32,20 @@ def handle_user_events():
         if event.type == pygame.QUIT:
             state["is_window_open"] = False
         elif state != consts.RUNNING_STATE:
-            continue
-        if event.type == pygame.K_UP:
             pass
-        elif event.type == pygame.K_DOWN:
-            pass
-        elif event.type == pygame.K_RIGHT:
-            pass
-        elif event.type == pygame.K_LEFT:
-            pass
-        elif event.type == pygame.K_KP_ENTER:
-            screen.draw_night_window()
+        if event.type == pygame.KEYDOWN:
+
+            if event.key == pygame.K_RETURN:
+                state['night'] = True
+
+            if event.key == pygame.K_UP:
+                pass
+            elif event.key == pygame.K_DOWN:
+                pass
+            elif event.key == pygame.K_RIGHT:
+                pass
+            elif event.key == pygame.K_LEFT:
+                pass
 
 
 def is_lose():
