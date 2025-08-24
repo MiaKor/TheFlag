e1b316363a157994be6aad4af019c23ec48adfdd
hello <ayab1142007@gamil.com>
2025-08-24 16:07:10 +0300

    created 2 functions

--- game_field.py
+++ game_field.py
@@ -8,19 +8,23 @@ def create_field():
     for i in range (consts.SCREEN_GRID_START_ROWS):
         for j in range (consts.SCREEN_GRID_COLS):
             game_field.append([])
+    return game_field
 
-# mines_positions = []
-# def random_positions():
-#     g = create_field()
-#     for i in len()
-
-    # random.choices()
 
 def randon_mines():
-    for i in range(consts.BUSHES_AMOUNT):
+    g = create_field()
+    for i in range(consts.MINE_AMOUNT):
         x = random.randint(0,consts.WINDOW_WIDTH)
+        while (x%50)!=0:
+            x = random.randint(0, consts.WINDOW_WIDTH)
         y = random.randint(0,consts.WINDOW_HEIGHT)
-        screen.blit(consts.MINE, (x,y))
+        while (y%25)!=0:
+            y = random.randint(0, consts.WINDOW_HEIGHT)
+
+        for j in range (3):
+            g.insert([x+j*50][y], consts.MINE)
+    return g
+
 
 
 
