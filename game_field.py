4db5b2e15162afe7a6671ba7c056e58d17dafa79
Mia koren <miakorenk2@gmail.com>
2025-08-25 10:34:17 +0300

    pos tuple list

--- game_field.py
+++ game_field.py
@@ -3,37 +3,34 @@ import consts
 import random
 import screen
 
+
 def create_field():
     game_field = []
 
-    for i in range (consts.SCREEN_GRID_START_ROWS):
+    for i in range(consts.SCREEN_GRID_START_ROWS):
         row = []
         game_field.append(row)
-        for j in range (consts.SCREEN_GRID_COLS):
+        for j in range(consts.SCREEN_GRID_COLS):
             row.append(' ')
     return game_field
 
 
 def randon_mines(game_filed):
-    tuple_list=[]
+    tuple_list = []
     for i in range(consts.MINE_AMOUNT):
-        x = random.randint(0, consts.SCREEN_GRID_START_ROWS-1)
-        y = random.randint(0, consts.SCREEN_GRID_COLS-1)
-        tuple_list.append((x,y))
+        x = random.randint(0, consts.SCREEN_GRID_START_ROWS - 1)
+        y = random.randint(0, consts.SCREEN_GRID_COLS - 1)
+        tuple_list.append((x, y))
     return tuple_list
 
     #    g = create_field()
-   # x = random.randint(0,consts.WINDOW_WIDTH)
-   #      while (x%50)!=0:
-   #          x = random.randint(0, consts.WINDOW_WIDTH)
-   #      y = random.randint(0,consts.WINDOW_HEIGHT)
-   #      while (y%25)!=0:
-   #          y = random.randint(0, consts.WINDOW_HEIGHT)
-   #
-   #      for j in range (3):
-   #          g.insert([x+j*50][y], consts.MINE)
-   #  return g
-
-
-
-
+# x = random.randint(0,consts.WINDOW_WIDTH)
+#      while (x%50)!=0:
+#          x = random.randint(0, consts.WINDOW_WIDTH)
+#      y = random.randint(0,consts.WINDOW_HEIGHT)
+#      while (y%25)!=0:
+#          y = random.randint(0, consts.WINDOW_HEIGHT)
+#
+#      for j in range (3):
+#          g.insert([x+j*50][y], consts.MINE)
+#  return g
